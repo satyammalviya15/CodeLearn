a44b79d67b28c55f6fb201986d9c0d6c820f2038
Satyam Malviya <satyammalviya281@gmail.com>
2025-07-28 23:30:54 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),
+            tailwindcss()
+  ],
+  theme:{
+    extend:{
+      fontSize :{
+        'course-details-heading-small':['26px','36px'],
+        'course-details-heading-large':['36px','44px'],
+        'home-heading-small':['28px','34px'],
+        'home-heading-large':['48px','56px'],
+        'default':['15px','21px']
+      },
+      gridTemplateColumns:{
+        auto :'repeat(auto-fit, minmax(200px, 1fr))'
+      },
+      spacing: {
+        'section-height':'500px'
+      },
+      maxWidth:{
+        'course-card' : '424px'
+      },
+      boxShadow:{
+        'custom-card': '0px 4px 15px 2px rgba(0,0,0,0.1)'
+      }
+    },
+  },
+})
