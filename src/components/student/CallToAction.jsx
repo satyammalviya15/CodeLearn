f1e0512c3f9005a9543e1eab4066f8ead2cf0e0c
Satyam Malviya <satyammalviya281@gmail.com>
2025-08-02 11:27:49 +0530

    logoupdate

--- src/components/student/CallToAction.jsx
+++ src/components/student/CallToAction.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { assets } from '../../assets/assets';
+import { Link } from 'react-router-dom';
+Link
 
 function CallToAction() {
   return (
@@ -7,8 +9,10 @@ function CallToAction() {
       <h1 className='text-xl md:text-4xl text-gray-800 font-semibold'>Learn anything, anytime ,anywhere</h1>
       <p className='text-gray-500 sm:text-sm'>Incididunt sint fugiat pariatur cupidatat consectetur sit cillum anim id veniam aliqua proident excepteur commodo do ea.</p>
       <div className='flex items-center font-medium gap-6 mt-4'>
-        <button className='px-10 py-3 rounded-md text-white bg-blue-600'>Get Started</button>
-        <button className='flex items-center gap-2'>Learn more<img src={assets.arrow_icon} alt='arrow_icon'/></button>
+          <Link className='px-10 py-3 rounded-md text-white bg-blue-600' to={'course-list'} onClick={()=>{scrollTo(0,0)}}>
+          Get Started
+          </Link> 
+        <Link className='flex items-center gap-2' to={'course-list'} onClick={()=>{scrollTo(0,0)}}>Learn more<img src={assets.arrow_icon} alt='arrow_icon'/></Link>
       </div>
     </div>
   )
